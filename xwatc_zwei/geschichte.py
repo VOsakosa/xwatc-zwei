e232ee5cb595d026cfa363add6e4aa8ddeae4dc7
Jasper Ischebeck <jasper.ischebeck@gmail.com>
2024-09-07 19:34:37 +0200

    Vorschlag für scenario.cfg

--- xwatc_zwei/geschichte.py
+++ xwatc_zwei/geschichte.py
@@ -1,1 +1,21 @@
-"""Speichert den Zustand des Spiels"""
+"""Die einzelnen Befehle innerhalb einer Geschichte"""
+from attrs import define
+
+Bedingung = str
+Item = str
+
+@define
+class Attribute:
+    if_: Bedingung
+
+@define
+class Text:
+    attribute: Attribute
+    text: str
+
+@define
+class Erhalten:
+    objekt: Item
+    anzahl: int = 1
+
+Zeile = Text | Erhalten
--- xwatc_zwei/verteiler.py
+++ xwatc_zwei/verteiler.py
@@ -1,1 +1,22 @@
 """Die Verteiler wählen Geschichtsmodule"""
+from attrs import define
+from xwatc_zwei.geschichte import Zeile
+
+
+@define
+class Geschichtsmodul:
+    id: str
+    zeilen: list[Zeile]
+
+
+@define
+class Verteiler:
+    """"""
+    module: list[Geschichtsmodul]
+
+
+@define
+class Hauptgeschichte:
+    """"""
+    start: Geschichtsmodul
+    name: str
