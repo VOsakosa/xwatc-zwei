a34797d80282bdc335369da5081f62e7d1169e77
Jasper Ischebeck <jasper.ischebeck@gmail.com>
2024-09-14 09:42:48 +0200

    Fähigkeiten für Mänx

--- tests/test_mänx.py
+++ tests/test_mänx.py
@@ -8,3 +8,22 @@ class TestMänx(unittest.TestCase):
         self.assertEqual(Mänx.default().get_wert("stabil"), 10)
         with self.assertRaises(KeyError):
             Mänx.default().get_wert("blubb")
+    
+    def test_get_set_fähigkeit(self):
+        m = Mänx.default()
+        self.assertEqual(m.get_fähigkeit("fliegen"), 0)
+        m.set_fähigkeit("fliegen", 2)
+        with self.assertRaises(ValueError):
+            m.set_fähigkeit("fliegen", -1)
+        with self.assertRaises(ValueError):
+            m.set_fähigkeit("fliegen", 6)
+        with self.assertRaises(ValueError):
+            m.set_fähigkeit("fliegen", -80)
+        self.assertEqual(m.get_fähigkeit("fliegen"), 2)
+        self.assertEqual(m.get_fähigkeit("liegen"), 0)
+
+        m.set_fähigkeit("fliegen", 5)
+        self.assertEqual(m.get_fähigkeit("fliegen"), 5)
+        m.set_fähigkeit("fliegen", 0)
+        self.assertEqual(m.get_fähigkeit("fliegen"), 0)
+
--- xwatc_zwei/mänx.py
+++ xwatc_zwei/mänx.py
@@ -6,20 +6,32 @@ from attrs import define, Factory
 T = TypeVar("T")
 VarTyp = bool | int | str
 
+
 @define
 class Mänx:
     """Der Hauptcharakter"""
     ATTRIBUTE: ClassVar[list[str]] = ["flink", "weise", "schlau", "stark", "wach"]
     P_WERTE: ClassVar[list[str]] = ["selbstsicher", "stabil", "gesellig"]
     _werte: dict[str, int]
+    _fähigkeiten: dict[str, int] = Factory(dict)
 
     @classmethod
     def default(cls) -> Self:
         return cls({w: 10 for w in (*cls.ATTRIBUTE, *cls.P_WERTE)})
-    
+
     def get_wert(self, name: str) -> int:
         """Attribut oder P-Wert des Mänxen holen."""
         return self._werte[name]
+
+    def set_fähigkeit(self, fähigkeit: str, stufe: int) -> None:
+        """Setze eine Fähigkeit auf eine feste Stufe."""
+        if not 0 <= stufe <= 5:
+            raise ValueError("Fähigkeiten müssen zwischen 0 und 5 sein.")
+        self._fähigkeiten[fähigkeit] = stufe
+
+    def get_fähigkeit(self, fähigkeit: str) -> int:
+        """Fähigkeitsstufe."""
+        return self._fähigkeiten.get(fähigkeit, 0)
 
 
 @define
@@ -31,6 +43,6 @@ class Welt:
         ans = self._variablen.get(variable)
         self._variablen[variable] = wert
         return ans
-    
+
     def get_variable(self, variable: str, default: T) -> VarTyp | T:
         return self._variablen.get(variable, default)
